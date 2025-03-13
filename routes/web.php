acde1f9c7eef61bd28245bbdfbf77fe8adb4b493
Luan Ibanhez <luanibanhez@gmail.com>
2025-03-13 13:00:51 -0300

    redirect rote login

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,7 @@
 use App\Livewire\Jogos;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 Route::get('/home',Jogos\Index::class)->name('home');
 
-Route::get('/login', Jogos\Login::class)->name('login');
+Route::get('/', Jogos\Login::class)->name('login');
